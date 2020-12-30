988cc151277859d650458e0e90fc2c323c7d4f38
zgaaa <1924297895@qq.com>
2020-12-30 14:22:57 +0800

    master

--- test1.go
+++ test1.go
@@ -2,16 +2,30 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
+// db全局变量
+var db *sql.DB
+
+func initDB() (err error) {
 	dsn := "root:root@tcp(127.0.0.1:3306)/myemployees"
-	db, err := sql.Open("mysql", dsn)
+	db, err = sql.Open("mysql", dsn)
 	if err != nil {
-		panic(err)
+		return err
 	}
-	defer db.Close()
-	
+	err = db.Ping()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func main() {
+	err := initDB()
+	if err != nil {
+		fmt.Printf("连接失败 err = %v", err)
+	}
+
 }
